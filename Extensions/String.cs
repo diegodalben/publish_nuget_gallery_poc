dd6381524fc71f155da1ba5b3d6b32619230c6f8
ddalben <ddalben@beatrix.co>
2021-07-28 14:38:58 -0300

    Added Extensions project

---
+++ Extensions/String.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Extensions
+{
+    public static class String
+    {
+        public static Guid GenerateGuid(this string value)
+        {
+            using var md5 = MD5.Create();
+            var valueBytes = Encoding.UTF8.GetBytes(value);
+            var hash = md5.ComputeHash(valueBytes);
+            return new Guid(hash);
+        }
+    }
+}
